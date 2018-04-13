efc32b8ab3e6c1f319380dbe090151690a1f146d
shrkddms <38336511+shrkddms@users.noreply.github.com>
2018-04-13 14:01:33 +0900

    Update MainActivity.java
    .

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -7,13 +7,12 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     
-    private TextView textView;
     private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.textView);
         button = findViewById(R.id.button);
 
     }
