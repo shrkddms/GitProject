f56d58b5d0d4a0960f2384cdc370cc025f6d738b
shrkddms <shrkddms@naver.com>
2018-04-13 14:48:03 +0900

    텍스트뷰 내용 변경

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
         Button button = findViewById(R.id.button);
+        textView.setText("안녕");
 
     }
 }
